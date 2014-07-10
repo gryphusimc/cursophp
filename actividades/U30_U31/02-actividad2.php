9f3874d7fd37817b6da49486525afec195174f6e
Jesús Ruiz-Ayúcar <chuso@gnoma.es>
2014-07-10 02:00:05 +0200

    Corregido ejercicio 2.
    Había un error con la precisión de los valores decimales.

--- actividades/U30_U31/02-actividad2.php
+++ actividades/U30_U31/02-actividad2.php
@@ -1,5 +1,5 @@
 <?php
-	$precio = 1.2;
+	$precio = 1.27;
 	$pagado = 2;
 
 	$disponible = [500,200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01];
@@ -13,6 +13,6 @@
 			$i++;
 		}
 
-		//echo $disponible[$i]."<br />";
-		$cambio = $cambio - $disponible[$i];
-	} while ($cambio > $disponible[count($disponible) - 1]);
+		echo $disponible[$i].PHP_EOL;
+		$cambio = round($cambio - $disponible[$i], 2);
+	} while ($cambio > 0);
